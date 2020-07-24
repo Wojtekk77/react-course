cd2fe74721a15cba4dcec0a391d163287036ded7
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2020-07-24 14:54:27 +0200

    list/array end good way to filter

--- array_table/App.js
+++ array_table/App.js
@@ -1,22 +1,55 @@
-const Item = (props) => <li>{`owoc ${props.content}`}</li>;
+const data = {
+  users: [
+    { id: 1, age: 29, name: "Ann", sex: "female" },
+    { id: 2, age: 49, name: "Martin", sex: "male" },
+    { id: 3, age: 19, name: "Stan", sex: "male" },
+    { id: 4, age: 11, name: "Karl", sex: "female" },
+  ],
+};
+const Item = ({ user }) => (
+  <div className="userInfo">
+    <h1>Użytkownik {user.name}</h1>
+    <p>informacje o użytkowniku</p>
+    <p>wiek użytkownika: {user.age}</p>
+    <p>płeć użytkownika: {user.sex}</p>
+  </div>
+);
 class ListItems extends React.Component {
   state = {
-    items: ["apple", "plum", "pear"],
+    select: "all",
   };
+
+  handleUsersSelect = (choice) => {
+    this.setState({
+      select: choice,
+    });
+  };
+  userList = () => {
+    const { select } = this.state;
+    let users = this.props.data.users;
+    if (this.state.select != "all") {
+      users = users.filter((user) => user.sex == select);
+    }
+    users = users.map((user) => <Item key={user.id} user={user} />);
+    return users;
+  };
+
   render() {
-    const Items = this.state.items.map((item) => (
-      <Item key={item} content={item} />
-    ));
     return (
       <>
-        <ul>
-          {this.state.items.map((item) => (
-            <Item key={item} content={item} />
-          ))}
-        </ul>
+        <div>
+          <button onClick={() => this.handleUsersSelect("all")}>Wszyscy</button>
+          <button onClick={() => this.handleUsersSelect("female")}>
+            Kobiety
+          </button>
+          <button onClick={() => this.handleUsersSelect("male")}>
+            Mężczyźni
+          </button>
+          {this.userList()}
+        </div>
       </>
     );
   }
 }
 
-ReactDOM.render(<ListItems />, document.getElementById("root"));
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
