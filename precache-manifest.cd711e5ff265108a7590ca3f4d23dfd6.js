0a0513a766b1bcc0f4ad15c8834954e3a6f1888a
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2020-08-02 16:07:22 +0200

    Updates

---
+++ precache-manifest.cd711e5ff265108a7590ca3f4d23dfd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6b4c2675871aa7ebfce83da0b38821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca6ad96beaedfe16064",
+    "url": "/static/css/main.6c87fdf3.chunk.css"
+  },
+  {
+    "revision": "55fe142b351f02b76a6b",
+    "url": "/static/js/2.d42db841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d42db841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca6ad96beaedfe16064",
+    "url": "/static/js/main.90ab48e7.chunk.js"
+  },
+  {
+    "revision": "32869580d4d8863656aa",
+    "url": "/static/js/runtime-main.0f7e687a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
