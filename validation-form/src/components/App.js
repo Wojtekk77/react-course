43ba3882d6838e3beaf089098527def78b20729c
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2020-08-14 15:53:00 +0200

    my own validation

--- validation-form/src/components/App.js
+++ validation-form/src/components/App.js
@@ -2,103 +2,181 @@ import React from "react";
 import logo from "../logo.svg";
 import "./App.css";
 
-const SquareField = (props) => {
-  return (
-    <div className={props.classCss}>
-      {props.win ? <span>w</span> : <span>f</span>}
-    </div>
-  );
-};
-
 class App extends React.Component {
   state = {
-    // 0 - red, 1 - black
-    colors: [0, 1],
-    result: [],
-    withoutWin: 0,
-    percentWins: 0,
-    cash: 1000,
-  };
-  //set list of colors and decide if we bet
-  //return color or null
-  makeBet = (previousColors, minRow, maxRow) => {
-    if (previousColors.length > maxRow) {
-      let arrOfLastBets = previousColors.slice(
-        Math.max(previousColors.length - maxRow, 0)
-      );
-      for (let i = maxRow - 1; i >= 0; i--) {
-        if (arrOfLastBets[i]) {
-        } else {
-          return null;
-        }
-      }
-      console.log(arrOfLastBets);
-      return arrOfLastBets[arrOfLastBets.length - 1].color;
-    }
-    return null;
+    username: "",
+    email: "",
+    pswd: "",
+    accept: true,
+    message: "",
+
+    errors: {
+      username: false,
+      email: false,
+      pswd: false,
+      accept: false,
+    },
   };
 
-  handleClick = () => {
-    const arr = [...this.state.colors];
-    var randomResults = [];
-    let withoutWin = 0;
-    let winInRow = 0;
-    let maxWithoutWin = 0;
-    let previousColor = 1;
-    let countWin = 0;
-    let numberOfTries = 1000;
-    for (let i = 0; i < numberOfTries; i++) {
-      let randomColor = arr[Math.floor(Math.random() * arr.length)];
-      this.makeBet(randomResults, 2, 8);
-      if (previousColor !== randomColor) {
-        withoutWin++;
-        winInRow = 0;
-      } else {
-        withoutWin = 0;
-        winInRow++;
-        countWin++;
-      }
+  messages = {
+    username_incorrect: "Name has to be longer than 9 characters",
+    email_incorrect: "There is no @ in email",
+    pswd_incorrect: "Password has to be longer than 8 characters",
+    accept_incorrect: "There is no acceptance",
+  };
 
-      randomResults = randomResults.concat({
-        id: i,
-        color: randomColor,
-        win: previousColor === randomColor,
-        winInRow: winInRow,
-        loseInRow: withoutWin,
+  handleChange = (e) => {
+    console.log(e.target.value);
+    console.log(e.target.type);
+    console.log(e.target.checked);
+    const name = e.target.name;
+    const value = e.target.value;
+    const type = e.target.type;
+    const accept = e.target.checked;
+    if (type === "checkbox") {
+      this.setState({
+        [name]: accept,
       });
-      previousColor = randomColor;
-      if (withoutWin > maxWithoutWin) {
-        maxWithoutWin = withoutWin;
-      }
+    } else {
+      this.setState({
+        [name]: value,
+      });
     }
-    this.setState({
-      result: randomResults,
-      withoutWin: maxWithoutWin,
-      percentWins: countWin / numberOfTries,
-    });
   };
 
+  handleSumbit = (e) => {
+    e.preventDefault();
+    console.log("dziala");
+    const validation = this.formValidation();
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        pswd: "",
+        accept: true,
+        message: "your form has been submitted",
+
+        errors: {
+          username: false,
+          email: false,
+          pswd: false,
+          accept: false,
+        },
+      });
+    } else {
+      this.setState({
+        message: "your form hasn't been valid",
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          pswd: !validation.pswd,
+          accept: !validation.accept,
+        },
+      });
+    }
+  };
+
+  formValidation = () => {
+    //true ok
+    //false !ok
+    let username = false;
+    let email = false;
+    let pswd = false;
+    let accept = false;
+    let correct = false;
+    if (
+      this.state.username.length > 10 &&
+      this.state.username.indexOf(" ") === -1
+    ) {
+      username = true;
+    }
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+    if (this.state.pswd.length > 9) {
+      pswd = true;
+    }
+    if (this.state.accept) {
+      accept = true;
+    }
+    if (username && email && pswd && accept) {
+      correct = true;
+    }
+    return {
+      username,
+      email,
+      pswd,
+      accept,
+      correct,
+    };
+  };
+  componentDidUpdate() {
+    console.log("znikl");
+    if (this.state.message !== "") {
+      setTimeout(() => {
+        this.setState({ message: "" });
+      }, 1000);
+    }
+  }
   render() {
-    const squareFields = this.state.result.map((item) => {
-      const squerColor = item.color ? "square-black" : "square-red";
-      return (
-        <SquareField
-          key={item.id}
-          classCss={squerColor}
-          win={item.win}
-          winInRow={item.winInRow}
-          loseInRow={item.loseInRow}
-        />
-      );
-    });
     return (
       <>
-        <button onClick={this.handleClick}>dawaj mi tutaj losowo</button>
-        {this.state.percentWins}
-        <span> </span>
-        {this.state.withoutWin}
-        <br />
-        {squareFields}
+        <form onSubmit={this.handleSumbit} noValidate>
+          <label htmlFor="name">
+            your name
+            <input
+              type="text"
+              id="name"
+              name="username"
+              onChange={this.handleChange}
+              value={this.state.username}
+            />
+            {this.state.errors.username && (
+              <span>{this.messages.username_incorrect}</span>
+            )}
+          </label>
+          <label htmlFor="email">
+            your email
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+            {this.state.errors.email && (
+              <span>{this.messages.email_incorrect}</span>
+            )}
+          </label>
+          <label htmlFor="password">
+            your password
+            <input
+              type="password"
+              id="password"
+              name="pswd"
+              onChange={this.handleChange}
+              value={this.state.pswd}
+            />
+            {this.state.errors.pswd && (
+              <span>{this.messages.pswd_incorrect}</span>
+            )}
+          </label>
+          <label htmlFor="accept">
+            agreement
+            <input
+              type="checkbox"
+              id="accept"
+              name="accept"
+              onChange={this.handleChange}
+              checked={this.state.accept}
+            />
+            {this.state.errors.accept && (
+              <span>{this.messages.accept_incorrect}</span>
+            )}
+          </label>
+          <button>submit</button>
+        </form>
+        {this.state.message && <h2>{this.state.message}</h2>}
       </>
     );
   }
