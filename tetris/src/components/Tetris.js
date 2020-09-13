dcad81aca94c178bd4fde7c980a48702f97fd35e
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2020-09-13 12:13:42 +0200

    tetris v2

--- tetris/src/components/Tetris.js
+++ tetris/src/components/Tetris.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { createStage } from '../gameHelpers';
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+// import { createStage } from "../gameHelpers";
+//styled components
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
+
+//custom hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
 
 // Components
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("re-render");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
           <div>
             <Display text="Score" />
